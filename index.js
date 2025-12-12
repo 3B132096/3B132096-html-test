383803a486745ed6bc03c8a6f0a0da0155a331fa
3B132096 <3B132096@gm.student.ncut.edu.tw>
2025-12-12 12:00:56 +0800

    JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("txt").textContent = "你好"
+
+const p = document.createElement("p");
+p.textContent = "動態文字段落";
+document.body.appendChild(p);
+
+document.getElementById("heading").innerHTML = "<h1> 標題 </h1>"
---
+++ oo.js
@@ -0,0 +1,6 @@
+var user = { name: 'KD', age: '37' };
+
+user.name = "Curry";
+user.age = "37";
+
+document.getElementById("user").innerHTML = user.name + "<br>" + user.age + "歲<br>";
